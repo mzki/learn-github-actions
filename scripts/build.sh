90b4f904578a01065edfbb64d0c658f6120440a6
mzki <28139969+mzki@users.noreply.github.com>
2022-11-14 01:58:22 +0900

    Add cross compile and release script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,13 @@ set -eu
 
 target=$1
 output=$2
+os=${3:-linux}
 echo $(ls .)
 echo $(pwd)
 go get -d -v ./...
-go build -o $output -v $target 
+if [ $os = "linux" ]; then
+  GOOS=linux go build -o $output -v $target 
+fi
+if [ $os = "windows" ]; then
+  GOOS=windows go build -o $output -v $target 
+fi
